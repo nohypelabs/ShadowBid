46bfb82a4c865b734826161696a5ed1d36eee24e
agds-alt <agdscid@gmail.com>
2026-05-27 00:43:46 +0700

    deploy the contract

--- frontend/src/constants/contracts.ts
+++ frontend/src/constants/contracts.ts
@@ -1,6 +1,6 @@
 import ShadowBidArtifact from "./ShadowBid.json";
 
 // Deployed on Arbitrum Sepolia
-export const SHADOWBID_ADDRESS = "0xF801Bb64c6f396e431ad0C3b8D8770BC028fF0D1" as const;
+export const SHADOWBID_ADDRESS = "0x2BccEa43CE4D32dbfE813c5FEdd39C396E75072c" as const;
 
 export const SHADOWBID_ABI = ShadowBidArtifact.abi;
